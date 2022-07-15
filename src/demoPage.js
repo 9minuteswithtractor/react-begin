db692600005406fcace4422526465ada54767d6d
Peter_V <petervil@protonmail.com>
2022-07-15 12:14:30 +0300

    initialize react-demo-page  wthout using create-react-app

---
+++ src/demoPage.js
@@ -0,0 +1,10 @@
+class Demo extends React.Component {
+  render() {
+    return (
+      <div className="demo-heading">
+        <h1>Demo page startup</h1>
+        <hr />
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Demo />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
